a62463d7634b92bc66b8d32eb74dd97d1ff5fa8e
tranduy1dol <tmd.id2004@gmail.com>
2024-09-04 11:44:04 +0000

    feat: aptos l1 service

--- crates/node/src/service/l1.rs
+++ crates/node/src/service/l1.rs
@@ -10,10 +10,12 @@ use starknet_api::core::ChainId;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::task::JoinSet;
+use aptos_sdk::types::account_address::AccountAddress;
 
 #[derive(Clone)]
 pub struct L1SyncService {
     db_backend: Arc<MadaraBackend>,
+    aptos_client: Option<dc_aptos::client::AptosClient>,
     eth_client: Option<EthereumClient>,
     l1_gas_provider: GasPriceProvider,
     chain_id: ChainId,
@@ -34,7 +36,7 @@ impl L1SyncService {
         devnet: bool,
         mempool: Arc<Mempool>,
     ) -> anyhow::Result<Self> {
-        let eth_client = if !config.sync_l1_disabled && (config.l1_endpoint.is_some() || !devnet) {
+        let eth_client = if config.l1_type == L1Type::Ethereum && !config.sync_l1_disabled {
             if let Some(l1_rpc_url) = &config.l1_endpoint {
                 let core_address = Address::from_slice(l1_core_address.as_bytes());
                 let l1_block_metrics = L1BlockMetrics::register().expect("Registering metrics");
@@ -52,6 +54,24 @@ impl L1SyncService {
             None
         };
 
+        let aptos_client = if config.l1_type == L1Type::Aptos && !config.sync_l1_disabled {
+            if let Some(l1_rpc_url) = &config.l1_endpoint {
+                let core_address = AccountAddress::from_hex_literal(config.clone().aptos_core_contract.unwrap().as_str())?;
+                let l1_block_metrics = dc_aptos::client::L1BlockMetrics::register(&metrics_handle)?;
+                Some(
+                    dc_aptos::client::AptosClient::new(l1_rpc_url.clone(), core_address, l1_block_metrics)
+                        .await
+                        .context("Creating aptos client")?,
+                )
+            } else {
+                anyhow::bail!(
+                    "No Ethereum endpoint provided. You need to provide one using --l1-endpoint <RPC URL> in order to verify the synced state or disable the l1 watcher using --no-l1-sync."
+                );
+            }
+        } else {
+            None
+        };
+
         // Note: gas price should be synced in case the madara is running in sequencer mode,
         // we haven't set any fix price for the gas, hence gas price should be none
         let gas_price_sync_enabled =
@@ -59,19 +79,30 @@ impl L1SyncService {
         let gas_price_poll = config.gas_price_poll;
 
         if gas_price_sync_enabled {
-            let eth_client = eth_client
-                .clone()
-                .context("L1 gas prices require the ethereum service to be enabled. Either disable gas prices syncing using `--gas-price 0`, or disable L1 sync using the `--no-l1-sync` argument.")?;
-            // running at-least once before the block production service
-            tracing::info!("⏳ Getting initial L1 gas prices");
-            mc_eth::l1_gas_price::gas_price_worker_once(&eth_client, l1_gas_provider.clone(), gas_price_poll)
-                .await
-                .context("Getting initial ethereum gas prices")?;
+            if eth_client.is_some() {
+                let eth_client = eth_client
+                    .clone()
+                    .context("L1 gas prices require the ethereum service to be enabled. Either disable gas prices syncing using `--gas-price 0`, or disable L1 sync using the `--no-l1-sync` argument.")?;
+                // running at-least once before the block production service
+                tracing::info!("⏳ Getting initial L1 gas prices");
+                mc_eth::l1_gas_price::gas_price_worker_once(&eth_client, l1_gas_provider.clone(), gas_price_poll)
+                    .await
+                    .context("Getting initial ethereum gas prices")?;
+
+            }
+            if aptos_client.is_some() {
+                let aptos_client = aptos_client
+                    .clone()
+                    .context("AptosClient is required to start the l1 sync service but not provided.")?;
+                // running at-least once before the block production service
+                dc_aptos::l1_gas_price::gas_price_worker(&aptos_client, l1_gas_provider.clone(), gas_price_poll_ms).await?;
+            }
         }
 
         Ok(Self {
             db_backend: Arc::clone(db.backend()),
             eth_client,
+            aptos_client,
             l1_gas_provider,
             chain_id,
             gas_price_sync_disabled: !gas_price_sync_enabled,
@@ -88,6 +119,8 @@ impl Service for L1SyncService {
             self.clone();
 
         if let Some(eth_client) = self.eth_client.take() {
+            let L1SyncService { l1_gas_provider, chain_id, gas_price_sync_disabled, gas_price_poll_ms, .. } = self.clone();
+
             // enabled
 
             let db_backend = Arc::clone(&self.db_backend);
@@ -102,6 +135,25 @@ impl Service for L1SyncService {
                     mempool,
                 )
                 .await
+            });
+        }
+
+        if let Some(aptos_client) = self.aptos_client.take() {
+            let L1SyncService { l1_gas_provider, chain_id, gas_price_sync_disabled, gas_price_poll_ms, .. } = self.clone();
+
+            // enabled
+
+            let db_backend = Arc::clone(&self.db_backend);
+            join_set.spawn(async move {
+                dc_aptos::sync::l1_sync_worker(
+                    &db_backend,
+                    &aptos_client,
+                    chain_id.to_felt(),
+                    l1_gas_provider,
+                    gas_price_sync_disabled,
+                    gas_price_poll_ms,
+                )
+                    .await
             });
         }
 
